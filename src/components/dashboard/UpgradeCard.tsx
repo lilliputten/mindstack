a19d153f1477ccf58ed1b3d399be7a1ffee855f2
lilliputten <lilliputten@gmail.com>
2025-12-15 17:51:25 +0300

    Issue #39: Added translations for UpgradeCard.

--- src/components/dashboard/UpgradeCard.tsx
+++ src/components/dashboard/UpgradeCard.tsx
@@ -3,12 +3,15 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/Button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/Card';
 import { isDev } from '@/constants';
+import { useT } from '@/i18n';
 
 export function UpgradeCard({
   className,
   dontFlatten,
   onSidebar,
 }: TPropsWithClassName & { dontFlatten?: boolean; onSidebar?: boolean }) {
+  const t = useT();
+
   return (
     <Card
       className={cn(
@@ -20,10 +23,8 @@ export function UpgradeCard({
       )}
     >
       <CardHeader className="md:max-xl:px-4">
-        <CardTitle>Upgrade to Pro</CardTitle>
-        <CardDescription>
-          Unlock all features and get unlimited access to our support team.
-        </CardDescription>
+        <CardTitle>{t('UpgradeCard.Title')}</CardTitle>
+        <CardDescription>{t('UpgradeCard.Description')}</CardDescription>
       </CardHeader>
       <CardContent className="md:max-xl:px-4">
         <Button
@@ -35,7 +36,7 @@ export function UpgradeCard({
             onSidebar && 'bg-white text-theme-600 hover:bg-theme-700 hover:text-white',
           )}
         >
-          Upgrade
+          {t('UpgradeCard.Upgrade')}
         </Button>
       </CardContent>
     </Card>
