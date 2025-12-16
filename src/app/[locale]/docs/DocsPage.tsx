7cc0dfdd61207a33d3105cc259534df46fbd4cda
lilliputten <lilliputten@gmail.com>
2025-12-16 15:22:47 +0300

    Issue #39: Removed 'force-static' from static pages. Reorganized static pages' structure.

---
+++ src/app/[locale]/docs/DocsPage.tsx
@@ -0,0 +1,129 @@
+import { setRequestLocale } from 'next-intl/server';
+
+import { constructMetadata } from '@/lib/constructMetadata';
+import { getErrorText, getRandomHashString } from '@/lib/helpers';
+import { cn } from '@/lib/utils';
+import { MarkdownText } from '@/components/ui/MarkdownText';
+import { ScrollArea } from '@/components/ui/ScrollArea';
+import { ContentFooter, MaxWidthWrapper } from '@/components/shared';
+import {
+  availableTopicsRoute,
+  contactEmail,
+  cookiesRoute,
+  docsRoute,
+  isDev,
+  privacyRoute,
+  publicAddr,
+  rootRoute,
+  settingsRoute,
+  termsRoute,
+  versionInfo,
+} from '@/config';
+import { getT } from '@/i18n';
+import { defaultLocale, strictLocalesList, TAwaitedLocaleProps, TLocale } from '@/i18n/types';
+
+// export const dynamic = 'force-static';
+
+const saveScrollHash = getRandomHashString();
+
+type TDocsPageProps = TAwaitedLocaleProps;
+
+interface TDocsPagePropsWithContent extends TDocsPageProps {
+  content?: string;
+}
+
+async function getContentImport(locale: TLocale) {
+  switch (locale) {
+    // TODO: Add another languages
+    case 'en':
+    default:
+      return import('./DocsContentEn.md');
+  }
+}
+
+async function getContent(locale: TLocale) {
+  const imported = await getContentImport(locale);
+  return imported.default;
+}
+
+export async function generateMetadata({ params }: TAwaitedLocaleProps) {
+  const { locale } = await params;
+  const t = await getT({ locale });
+  return constructMetadata({
+    title: t('Pages.DocsTitle'),
+    locale,
+  });
+}
+
+export async function generateStaticParams() {
+  const locales = strictLocalesList;
+  const params = [];
+  for (const locale of locales) {
+    let content: string = '';
+    try {
+      content = await getContent(locale);
+    } catch (error) {
+      const message = 'Error loading page content for static generation';
+      const details = getErrorText(error);
+      // eslint-disable-next-line no-console
+      console.error('[DocsPage:generateStaticParams]', [message, details].join(': '), {
+        message,
+        details,
+        error,
+      });
+    }
+    params.push({ locale, content });
+  }
+  return params;
+}
+
+export async function DocsPage(props: TDocsPagePropsWithContent) {
+  const { params, content: preloadedContent } = props;
+  const { locale = defaultLocale } = await params;
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
+  let content = preloadedContent;
+
+  if (!content) {
+    content = await getContent(locale);
+  }
+
+  // Variables to render
+  const vars = {
+    availableTopicsRoute,
+    contactEmail,
+    cookiesRoute,
+    docsRoute,
+    privacyRoute,
+    publicAddr,
+    rootRoute,
+    settingsRoute,
+    termsRoute,
+    versionInfo,
+  };
+
+  return (
+    <ScrollArea
+      saveScrollKey="DocsContent"
+      saveScrollHash={saveScrollHash}
+      className={cn(
+        isDev && '__DocsContent_Scroll',
+        'flex flex-1 flex-col overflow-hidden',
+        'bg-theme-500/5',
+      )}
+      viewportClassName={cn(
+        isDev && '__DocsContent_ScrollViewport',
+        'flex flex-1 flex-col',
+        'bg-decorative-gradient',
+        '[&>div]:flex-col [&>div]:flex-1 [&>div]:justify-center [&>div]:items-center',
+      )}
+    >
+      <MaxWidthWrapper className="text-content flex flex-col p-6">
+        <MarkdownText vars={vars}>{content || ''}</MarkdownText>
+      </MaxWidthWrapper>
+      <ContentFooter />
+    </ScrollArea>
+  );
+}
---
+++ src/app/[locale]/terms/TermsPage.tsx
@@ -0,0 +1,113 @@
+import { setRequestLocale } from 'next-intl/server';
+
+import { constructMetadata } from '@/lib/constructMetadata';
+import { formatDate, getErrorText, getRandomHashString } from '@/lib/helpers';
+import { cn } from '@/lib/utils';
+import { MarkdownText } from '@/components/ui/MarkdownText';
+import { ScrollArea } from '@/components/ui/ScrollArea';
+import { ContentFooter, MaxWidthWrapper } from '@/components/shared';
+import { contactEmail, effectiveTermsDate, isDev, publicAddr } from '@/config';
+import { getT } from '@/i18n';
+import { defaultLocale, strictLocalesList, TAwaitedLocaleProps, TLocale } from '@/i18n/types';
+
+// export const dynamic = 'force-static';
+
+const saveScrollHash = getRandomHashString();
+
+type TTermsPageProps = TAwaitedLocaleProps;
+
+interface TTermsPagePropsWithContent extends TTermsPageProps {
+  content?: string;
+}
+
+async function getContentImport(locale: TLocale) {
+  switch (locale) {
+    case 'es':
+      return import('./TermsContentEs.md');
+    case 'ru':
+      return import('./TermsContentRu.md');
+    case 'en':
+    default:
+      return import('./TermsContentEn.md');
+  }
+}
+
+async function getContent(locale: TLocale) {
+  const imported = await getContentImport(locale);
+  return imported.default;
+}
+
+export async function generateMetadata({ params }: TAwaitedLocaleProps) {
+  const { locale } = await params;
+  const t = await getT({ locale });
+  return constructMetadata({
+    title: t('Pages.TermsTitle'),
+    locale,
+  });
+}
+
+export async function generateStaticParams() {
+  const locales = strictLocalesList;
+  const params = [];
+  for (const locale of locales) {
+    let content: string = '';
+    try {
+      content = await getContent(locale);
+    } catch (error) {
+      const message = 'Error loading page content for static generation';
+      const details = getErrorText(error);
+      // eslint-disable-next-line no-console
+      console.error('[TermsPage:generateStaticParams]', [message, details].join(': '), {
+        message,
+        details,
+        error,
+      });
+    }
+    params.push({ locale, content });
+  }
+  return params;
+}
+
+export async function TermsPage(props: TTermsPagePropsWithContent) {
+  const { params, content: preloadedContent } = props;
+  const { locale = defaultLocale } = await params;
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
+  let content = preloadedContent;
+
+  if (!content) {
+    content = await getContent(locale);
+  }
+
+  // Variables to render
+  const vars = {
+    effectiveDate: formatDate(effectiveTermsDate, locale),
+    contactEmail,
+    publicAddr,
+  };
+
+  return (
+    <ScrollArea
+      saveScrollKey="TermsContent"
+      saveScrollHash={saveScrollHash}
+      className={cn(
+        isDev && '__TermsContent_Scroll',
+        'flex flex-1 flex-col overflow-hidden',
+        'bg-theme-500/5',
+      )}
+      viewportClassName={cn(
+        isDev && '__TermsContent_ScrollViewport',
+        'flex flex-1 flex-col',
+        'bg-decorative-gradient',
+        '[&>div]:flex-col [&>div]:flex-1 [&>div]:justify-center [&>div]:items-center',
+      )}
+    >
+      <MaxWidthWrapper className="text-content flex flex-col p-6">
+        <MarkdownText vars={vars}>{content || ''}</MarkdownText>
+      </MaxWidthWrapper>
+      <ContentFooter />
+    </ScrollArea>
+  );
+}
