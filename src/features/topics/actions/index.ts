2933c1c17e0ebc9a1fe853d64e56dec6faec55fa
lilliputten <lilliputten@gmail.com>
2026-01-08 00:17:23 +0300

    Issue #49: Added categories database (with migration), added vercel blob and sharp dependencies, blob storage environment setting (`BLOB_READ_WRITE_TOKEN`, for image handling).

--- src/features/topics/actions/index.ts
+++ src/features/topics/actions/index.ts
@@ -1,7 +1,7 @@
 export * from './addNewTopic';
 export * from './deleteTopic';
 export * from './deleteTopics';
-export * from './getAllUsersTopics';
+// export * from './getAllUsersTopics';
 export * from './getAvailableTopicById';
 export * from './getAvailableTopics';
 export * from './getThisUserTopics';
