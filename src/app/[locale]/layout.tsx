a04bdff85400532e97b719bb8fe09bdaecb4c2d1
lilliputten <lilliputten@gmail.com>
2026-02-08 19:03:41 +0300

    Issue #77: Added logging of user sign-in event.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import { generateMetadata, generateStaticParams, RootLayout, viewport } from './RootLayout';
 
-export const dynamic = 'force-static';
+// export const dynamic = 'force-static'; // NOTE: It prevents update of dynamic data, like the current user status
 
 export { generateMetadata, generateStaticParams, viewport };
 export default RootLayout;
