ef5dbebeebe4ebf5b891d7edb4ffb10e57e237d4
lilliputten <lilliputten@gmail.com>
2026-02-09 23:46:33 +0300

    Issue #80: Implemented default topics filtering with the current locale language (if no locale has been set in the filters). Added special searchLanguage value `"-"` (dash) to search for all languages.

--- src/features/topics/components/AvailableTopicsFilters/AvailableTopicsFiltersInfo.tsx
+++ src/features/topics/components/AvailableTopicsFilters/AvailableTopicsFiltersInfo.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import React from 'react';
+import { useLocale } from 'next-intl';
 
 import { truncateString } from '@/lib/helpers';
 import { TPropsWithClassName } from '@/lib/types';
 import { cn } from '@/lib/utils';
-import { useT } from '@/i18n';
+import { TLocale, useT } from '@/i18n';
+import { LanguageName } from '@/components/shared';
 import { isDev } from '@/config';
 import {
   getActiveFilterIds,
@@ -24,59 +26,79 @@ export function AvailableTopicsFiltersInfo(props: TProps) {
   const { className, filtersData, maxValueLength = 30 } = props;
   // See texts aimed to be translated in the `src/contexts/TopicsFiltersContext/TopicsFiltersTexts.ts`
   const tTexts = useT('AvailableTopicsFilterTexts');
+
   const activeFilterIds = getActiveFilterIds(filtersData);
+
   const { categoryNames, isLoading: isCategoryNamesLoading } = useCategoryNames();
 
-  if (!activeFilterIds.length) {
-    return null;
-  }
+  const locale = useLocale() as TLocale;
 
   const categoryIds = filtersData?.categoryIds?.length ? filtersData?.categoryIds : undefined;
-  const convertedData = filtersData && {
-    ...filtersData,
-    categoryIds,
-    categoryNames: !isCategoryNamesLoading
-      ? categoryIds?.map((id) => categoryNames?.[id]).filter(Boolean)
-      : undefined,
-  };
+  const convertedData = React.useMemo(
+    () =>
+      filtersData && {
+        ...filtersData,
+        categoryIds,
+        categoryNames: !isCategoryNamesLoading
+          ? categoryIds?.map((id) => categoryNames?.[id]).filter(Boolean)
+          : undefined,
+      },
+    [categoryIds, categoryNames, filtersData, isCategoryNamesLoading],
+  );
 
-  const renderItems = activeFilterIds
-    .map((id) => {
-      const val = convertedData?.[id];
-      if (val == undefined || (Array.isArray(val) && !val.length)) {
-        return undefined;
-      }
-      if (id === 'categoryIds' && !convertedData?.categoryNames?.length) {
-        return undefined;
-      }
-      const { showOnlyValue, value } = getFiltersDataValueString(id, {
-        filtersData: convertedData,
-        specific: true,
-        t: tTexts,
-      });
-      const content = truncateString(value, maxValueLength);
-      return (
-        <span
-          key={id}
-          data-id={id}
-          className={cn(
-            isDev && '__AvailableTopicsFiltersInfo_Item', // DEBUG
-            'me-2',
-            '[&:not(:last-child)]:after:inline-block',
-            '[&:not(:last-child)]:after:ps-1',
-            '[&:not(:last-child)]:after:content-["|"]',
-            '[&:not(:last-child)]:after:opacity-15',
-            className,
-          )}
-        >
-          {!showOnlyValue && (
-            <span className="mr-1 inline-block opacity-50">{getFilterFieldName(id, tTexts)}:</span>
-          )}
-          <span>{content}</span>{' '}
-        </span>
-      );
-    })
-    .filter(Boolean);
+  const renderItems = React.useMemo(
+    () =>
+      activeFilterIds
+        .map((id) => {
+          const val = convertedData?.[id];
+          if (id != 'searchLang') {
+            if (val == undefined || (Array.isArray(val) && !val.length)) {
+              return undefined;
+            }
+            if (id === 'categoryIds' && !convertedData?.categoryNames?.length) {
+              return undefined;
+            }
+          }
+          const { showOnlyValue, value } = getFiltersDataValueString(id, {
+            filtersData: convertedData,
+            specific: true,
+            t: tTexts,
+          });
+          const content = truncateString(value, maxValueLength);
+          return (
+            <span
+              key={id}
+              data-id={id}
+              className={cn(
+                isDev && '__AvailableTopicsFiltersInfo_Item', // DEBUG
+                'me-2',
+                '[&:not(:last-child)]:after:inline-block',
+                '[&:not(:last-child)]:after:ps-1',
+                '[&:not(:last-child)]:after:content-["|"]',
+                '[&:not(:last-child)]:after:opacity-15',
+                className,
+              )}
+            >
+              {!showOnlyValue && (
+                <span className="mr-1 inline-block opacity-50">
+                  {getFilterFieldName(id, tTexts)}:
+                </span>
+              )}
+              {id === 'searchLang' ? (
+                value === '-' ? (
+                  tTexts('AnyLanguage')
+                ) : (
+                  <LanguageName langCode={val && typeof val === 'string' ? val : locale} />
+                )
+              ) : (
+                <span>{content}</span>
+              )}{' '}
+            </span>
+          );
+        })
+        .filter(Boolean),
+    [activeFilterIds, convertedData, tTexts, maxValueLength, className, locale],
+  );
 
   const hasFilters = !!renderItems.length;
 
